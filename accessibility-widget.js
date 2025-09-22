48502dbb1557cfc38117e41086a9d9b0a7fc3033
snm62 <sandra@seattlenewmedia.com>
2025-09-22 19:14:17 +0530

    Update accessibility-widget.js

--- accessibility-widget.js
+++ accessibility-widget.js
@@ -19293,10 +19293,7 @@ body.big-white-cursor * {
             const response = await fetch(apiUrl, {
                 method: 'GET',
                 headers: {
-                    'Content-Type': 'application/json',
-                    'Cache-Control': 'no-cache, no-store, must-revalidate',
-                    'Pragma': 'no-cache',
-                    'Expires': '0'
+                    'Content-Type': 'application/json'
                 }
             });
             
